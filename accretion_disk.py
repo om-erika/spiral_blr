7403d03c628b6fac6de041402bc021c1e89ad15b
om-erika <erikasottocorno9@gmail.com>
2025-04-28 18:14:48 +0200

    added doppler kernel

--- accretion_disk.py
+++ accretion_disk.py
@@ -16,6 +16,7 @@ sigma_B = 1.28e-23  # Stefan-Boltzmann constant (W/m^2/K^4)
 M_sun = 2e30  # Solar mass (kg)
 M = 2e30*1e8  # Mass of the black hole (kg)
 chi = 0.1  # Dimensionless spin parameter
+inc = np.radians(30)
 
 # Function to calculate the radiative efficiency of a black hole
 # Returns 1 adimensionless value
@@ -161,28 +162,25 @@ def generate_set_of_temperatures(times, Rs, thetas, M, f_edd, chi, R_in, tau, si
 # Function to de-evolve the phase of an orbiting object
 # Find where the emitting element of the accretion disc was at an arbitrary time
 # Modifies a pre-allocated CUDA device array (filled with zeros) with the de-evolved angles
-@cuda.jit
-def de_evolve(xi, theta, times, time_delay, initial_phase, M, d_thetaprimes):
-    idx_xi, idx_theta, idx_t = cuda.grid(3)
-
-    if idx_xi < xi.size and idx_theta < theta.size and idx_t < times.size:
-        d_thetaprimes[idx_xi][idx_theta][idx_t] =  ((theta[idx_theta] - (times[idx_t] -time_delay[idx_xi][idx_theta])*keplerian_freq(xi[idx_xi], M) + initial_phase)) % (2*np.pi)
+@cuda.jit(device = True)
+def de_evolve(xi, theta, time, initial_phase, M):
+    td = time_delay(xi, theta, inc)
+    kf = keplerian_freq(M, xi) 
+    return ((theta - (time -td)*kf + initial_phase)) % (2*math.pi)
 
 #Function to compute time delay
 # Modifies a pre-allocated CUDA device array (filled with zeros) with the de-evolved angles
 # @cuda.jit(device=True) would be more efficient if we don't need to save them in a device array
-@cuda.jit
-def compute_time_delay(xi, theta, inc, d_td):
-    idx_xi, idx_theta = cuda.grid(2)
-    if idx_xi < xi.size and idx_theta < theta.size:    
-        d_td[idx_xi][idx_theta] = xi[idx_xi]*(1+math.sin(inc)*math.cos(theta[idx_theta]))
+@cuda.jit(device=True)
+def time_delay(xi, theta, inc):
+    return xi*(1+math.sin(inc)*math.cos(theta))
 
 #x, y are separated cuda device
 @cuda.jit(device = True)
 def position_components(angle, R):
     x = R * math.sin(angle)  # x-coordinate
     y = R * math.cos(angle)  # y-coordinate
-    return x,y
+    return (x,y)
 
 #vx, vy are separated cuda device
 @cuda.jit(device = True)
@@ -192,33 +190,59 @@ def velocity_components(angle, R, M, corot=0):
     vy = -v_kep * math.sin(angle) # y-component of velocity
 
     if corot == 0:  # Corotating case
-        return vx, vy
+        return (vx, vy)
     else:  # Non-corotating case
-        return -vx, -vy
-    
-# Function to calculate the Doppler factor for relativistic motion
-# Returns the Doppler factor for the given angles and positions
-@cuda.jit(device = True)
-def Doppler_factor(v1, v2, r1, r2):
-    """
-    Calculate the Doppler factor for a moving source and observer.
-    Parameters:
-    v1 (numpy.ndarray): Velocity vector of the observer (in units of m/s).
-    v2 (numpy.ndarray): Velocity vector of the source (in units of m/s).
-    r1 (numpy.ndarray): Position vector of the observer (in units of m).
-    r2 (numpy.ndarray): Position vector of the source (in units of m).
-    Returns:
-    float: The Doppler factor, which accounts for the relativistic effects 
-           of motion on the observed frequency of a signal.
-    """
+        return (-vx, -vy)
 
-    v2_para = np.dot(v2, v1) * v1 / np.linalg.norm(v1)**2  # Parallel component of v2 
-    v2_perp = v2 - v2_para  # Perpendicular component of v2
-    gamma_2 = 1 / np.sqrt(1 - np.linalg.norm(v2)**2 / c**2)  # Lorentz factor for v2
-    beta = 1 / c * (v1 - v2 + 1 / gamma_2 * v2_perp) / (1 - np.dot(v1, v2) / c**2)
-    r_vers = (r2 - r1) / np.linalg.norm(r2 - r1)  # Unit vector in the direction of r2 - r1
-    gamma = np.sqrt(1 / (1 - beta**2))  # Lorentz factor for combined motion
-    D = 1 / (gamma * (1 - np.dot(beta, r_vers)))  # Doppler factor
+@cuda.jit(device = True)
+def keplerian_freq(M, R):
+    omega_k = math.sqrt(G * M / R**3)
+    return omega_k
+
+
+#kernel to compute doppler factor
+#need to do things like norm/dot explicitly
+#therefore I separated x and y components
+@cuda.jit(device=True)
+def doppler_factor(v1, v2, r1, r2):
+    #norms/dots v1, v2
+    norm_v1_sq = v1[0]*v1[0] + v1[1]*v1[1]
+    norm_v2_sq = v2[0]*v2[0] + v2[1]*v2[1]
+    v2_dot_v1 = v2[0]*v1[0] + v2[1]*v1[1]
+    norm_v1_minus_v2_sq = (v1[0]-v2[0])*(v1[0]-v2[0]) + (v1[1]-v2[1])*(v1[1]-v2[1])
+    
+    # Parallel components of v2
+    v2_para_x = (v2_dot_v1 / norm_v1_sq) * v1[0]
+    v2_para_y = (v2_dot_v1 / norm_v1_sq) * v1[1]
+
+    # Perpendicular components of v2
+    v2_perp_x = v2[0] - v2_para_x
+    v2_perp_y = v2[1] - v2_para_y
+
+    # Lorentz factor v2
+    gamma_2 = 1.0 / math.sqrt(1.0 - norm_v2_sq / (c**2))
+
+    # beta vector v2
+    beta_x = (1.0/c)*(v1[0] - v2[0] + (1.0/gamma_2)*v2_perp_x) / (1.0 - (v2_dot_v1)/(c**2))
+    beta_y = (1.0/c)*(v1[1] - v2[1] + (1.0/gamma_2)*v2_perp_y) / (1.0 - (v2_dot_v1)/(c**2))
+    beta_norm_sq = beta_x*beta_x + beta_y*beta_y
+ 
+    # np.dot(r2-r1)/(norm(r2-r1)**2)
+    dr_x = r2[0] - r1[0]
+    dr_y = r2[1] - r1[1]
+    norm_dr = math.sqrt(dr_x*dr_x + dr_y*dr_y)
+    r_vers_x = dr_x / norm_dr
+    r_vers_y = dr_y / norm_dr
+
+    #Lorentz factor v1-v2
+    gamma = math.sqrt(1.0 / (1.0 - beta_norm_sq))
+
+    # Beta dot r_vers
+    beta_dot_r_vers = beta_x*r_vers_x + beta_y*r_vers_y
+
+    # Doppler factor
+    D = 1.0 / (gamma * (1.0 - beta_dot_r_vers))
+
     return D
 
 # Function to calculate the innermost stable circular orbit (ISCO) radius
